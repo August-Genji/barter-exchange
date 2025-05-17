507f982d3b94ef3028f74a0333530aefab100b10
August <augustru4@gmail.com>
2025-05-17 05:54:28 +0300

    feat: Добавил поиск и фильтрцию по объявлениям и предложениям

--- barter_project/ads/views.py
+++ barter_project/ads/views.py
@@ -4,6 +4,9 @@ from .models import Ad, ExchangeProposal
 from .serializer import AdSerializer, UserSerializer, ExchangeProposalSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly, IsSenderOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.exceptions import PermissionDenied
 
 
 class UserViewSet(ReadOnlyModelViewSet):
@@ -16,12 +19,31 @@ class AdViewSet(ModelViewSet):
     serializer_class = AdSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'condition']
+    search_fields = ['title', 'description']
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        if response.data['count'] == 0:
+            response.data['message'] = 'По вашему запросы ничего не найдено ('
+        return response
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
 
 class ExchangeProposalViewSet(ModelViewSet):
     queryset = ExchangeProposal.objects.all()
     serializer_class = ExchangeProposalSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsSenderOrReadOnly]
 
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['status', 'ad_sender', 'ad_receiver']
+    search_fields = ['comment']
 
+    def perform_create(self, serializer):
+        ad_sender = serializer.validated_data['ad_sender']
+        if ad_sender.user != self.request.user:
+            raise PermissionDenied("Вы можете отправлять предложения только от своих объявлений")
+        serializer.save()
